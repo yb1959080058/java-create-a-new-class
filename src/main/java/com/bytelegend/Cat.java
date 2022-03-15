8f10e6e606fdfb6adf5b5b20d81123b1e2ad010a
DUST2333 <1119736882@qq.com>
2022-03-15 06:32:19 +0000

    Update at 2022-03-15T06:32:19.709385Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,13 +1,11 @@
-package cat;
+package com.bytelegend;
 
 /**
  * @author
  * @title: Cat
  * @projectName java-create-a-new-class-main
  * @description: TODO
- * @date 2022/3/1016:47
+ * @date 2022/3/1514:31
  */
 public class Cat {
-    
 }
-
