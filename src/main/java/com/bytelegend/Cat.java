00840ed8ba1f9d6109bc408be662aa8b46f60b25
onionwing <1069269649@qq.com>
2022-04-07 14:11:02 +0000

    Update at 2022-04-07T14:11:02.886042Z

---
+++ src/main/java/com/bytelegend/Cat.java
@@ -0,0 +1,17 @@
+public Cat {
+    private String name;
+    private int age;
+
+    void setName(String name){
+        this.name = name;
+    }
+    String getName(){
+        return this.name;
+    }
+    void setAge(int age){
+        this.age = age;
+    }
+    String getAge(){
+        return this.age;
+    }
+}
