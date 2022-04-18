4563dbbca2ad186453bd54c876a2b59d9dde9822
Azhigit <Azgithub@163.com>
2022-04-18 01:38:27 +0000

    Update at 2022-04-18T01:38:27.522472Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,8 +1,11 @@
 public class Cat{
     public static void main(String[] args){
         Cat cat = new Cat();
+        cat.name = "cat";
+        cat.age = 10;
     }
 }
 public class Cat {
-    new Cat();
+    String name;
+    int age;
 }
