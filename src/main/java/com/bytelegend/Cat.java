5f1cff3f5e42d82e801cbecffb706f1ca3ae7e23
Longyang-Liu <2452791011@qq.com>
2022-05-05 07:43:59 +0000

    Update at 2022-05-05T07:43:59.648690Z

---
+++ src/main/java/com/bytelegend/Cat.java
@@ -0,0 +1,24 @@
+package com.bytelegend;
+
+public class Cat{
+    public Long id;
+    public String name;
+
+    public String getId()
+    {
+        return id;
+    }
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
