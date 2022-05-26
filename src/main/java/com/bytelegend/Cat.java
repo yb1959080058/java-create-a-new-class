d68bc56eb9c3ef40be3c90c72726174300ff275b
a1212fff <werd134679@gmail.com>
2022-05-26 04:30:37 +0000

    Update at 2022-05-26T04:30:37.383898Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,4 +1,8 @@
 package com.bytelegend;
 
-public class cat {
+import com.bytelegend.Cat;
+public class Cat {
+    public static void main(String[] args) {
+        System.out.print("Hello,World!");
+    }
 }
