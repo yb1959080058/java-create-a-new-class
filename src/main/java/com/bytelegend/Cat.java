9bb68bed6def6bcd3a8399ce066294ddfcb77def
Birlinha <omenaribeiro13@gmail.com>
2022-07-11 00:02:27 +0000

    Update at 2022-07-11T00:02:27.090986Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
 public class Cat {
-    System.out.println("Hello World.");
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
 }
