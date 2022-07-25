bf103e0f6e91f7ea6ef3e27e6e5f189f5c142376
Jiu-Notte <284727826@qq.com>
2022-07-25 02:26:44 +0000

    Update at 2022-07-25T02:26:44.760100Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,10 +1,9 @@
 public class Cat{
-    String variety;
-    int price;
+    public String variety;
     public void eat(){
-        System.out.println("吃东西")；
+        System.out.println(variety+"吃东西")；
     }
-    public void sleep(){
+    public static void sleep(){
         System.out.println("睡觉")；
     }
 }
