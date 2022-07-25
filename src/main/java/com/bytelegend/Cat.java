5a026c8188dd28e8b8f3ae14ac71172e3da4d3d7
Jiu-Notte <284727826@qq.com>
2022-07-25 02:51:13 +0000

    Update at 2022-07-25T02:51:13.199110Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,4 +1,21 @@
-package bytelegend;
 public class Cat{
-   
+    private String name;
+    private int age;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getAge(int age){
+        this.age=age;
+    }
+   public void move(){
+       System.out.println(name+age+"走路")
+   }
+     @Override
+     public String toString(){
+         return "Cat{"+"name'"+name+'\''+",age"+age+'}';
+     }
 }
