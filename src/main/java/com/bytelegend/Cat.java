4dc14de54d36344d88a1ac12d7ba606ae5a1e134
LearningThinking <iwannanewlife@outlook.com>
2022-08-21 12:28:37 +0000

    Update at 2022-08-21T12:28:37.632462Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,1 +1,17 @@
-public class Cat{}
+public class Cat{
+    private String name;
+
+    public Cat(){}
+
+    public Cat(String name){
+        this.name = name;
+    }
+
+    public getName(){
+        return name;
+    }
+
+    public setName(String name){
+        this.name = name;
+    }
+}
