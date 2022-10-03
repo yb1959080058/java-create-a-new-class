9380f55f2275c2d313c43abaad7dceb30e324dde
QianChiLang1 <1149956184@qq.com>
2022-10-03 15:19:55 +0000

    Update at 2022-10-03T15:19:55.424994Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,3 +1,127 @@
+D:\programming\Java\jdk-17.0.2\bin\java.exe -Dmaven.multiModuleProjectDirectory=D:\programming\Java\java-create-a-new-class "-Dmaven.home=D:\programming\IntelliJ IDEA 2021.1.3\plugins\maven\lib\maven3" "-Dclassworlds.conf=D:\programming\IntelliJ IDEA 2021.1.3\plugins\maven\lib\maven3\bin\m2.conf" "-Dmaven.ext.class.path=D:\programming\IntelliJ IDEA 2021.1.3\plugins\maven\lib\maven-event-listener.jar" "-javaagent:D:\programming\IntelliJ IDEA 2021.1.3\lib\idea_rt.jar=58280:D:\programming\IntelliJ IDEA 2021.1.3\bin" -Dfile.encoding=UTF-8 -classpath "D:\programming\IntelliJ IDEA 2021.1.3\plugins\maven\lib\maven3\boot\plexus-classworlds-2.6.0.jar;D:\programming\IntelliJ IDEA 2021.1.3\plugins\maven\lib\maven3\boot\plexus-classworlds.license" org.codehaus.classworlds.Launcher -Didea.version=2021.1.3 verify -P aliyunMavenMirror
+[INFO] Scanning for projects...
+[INFO] 
+[INFO] --------------< ByteLegendQuest:java-create-a-new-class >---------------
+[INFO] Building java-create-a-new-class 1.0-SNAPSHOT
+[INFO] --------------------------------[ jar ]---------------------------------
+[INFO] 
+[INFO] --- maven-resources-plugin:2.6:resources (default-resources) @ java-create-a-new-class ---
+[INFO] Using 'UTF-8' encoding to copy filtered resources.
+[INFO] skip non existing resourceDirectory D:\programming\Java\java-create-a-new-class\src\main\resources
+[INFO] 
+[INFO] --- maven-compiler-plugin:3.1:compile (default-compile) @ java-create-a-new-class ---
+[INFO] Changes detected - recompiling the module!
+[INFO] Compiling 1 source file to D:\programming\Java\java-create-a-new-class\target\classes
+[INFO] 
+[INFO] --- maven-resources-plugin:2.6:testResources (default-testResources) @ java-create-a-new-class ---
+[INFO] Using 'UTF-8' encoding to copy filtered resources.
+[INFO] skip non existing resourceDirectory D:\programming\Java\java-create-a-new-class\src\test\resources
+[INFO] 
+[INFO] --- maven-compiler-plugin:3.1:testCompile (default-testCompile) @ java-create-a-new-class ---
+[INFO] Changes detected - recompiling the module!
+[INFO] Compiling 1 source file to D:\programming\Java\java-create-a-new-class\target\test-classes
+[INFO] 
+[INFO] --- maven-surefire-plugin:2.22.2:test (default-test) @ java-create-a-new-class ---
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/maven-surefire-common/2.22.2/maven-surefire-common-2.22.2.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/maven-surefire-common/2.22.2/maven-surefire-common-2.22.2.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/plugin-tools/maven-plugin-annotations/3.5.2/maven-plugin-annotations-3.5.2.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/plugin-tools/maven-plugin-annotations/3.5.2/maven-plugin-annotations-3.5.2.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/plugin-tools/maven-plugin-tools/3.5.2/maven-plugin-tools-3.5.2.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/plugin-tools/maven-plugin-tools/3.5.2/maven-plugin-tools-3.5.2.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-parent/31/maven-parent-31.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-parent/31/maven-parent-31.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/apache/19/apache-19.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/apache/19/apache-19.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-api/2.22.2/surefire-api-2.22.2.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-api/2.22.2/surefire-api-2.22.2.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-logger-api/2.22.2/surefire-logger-api-2.22.2.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-logger-api/2.22.2/surefire-logger-api-2.22.2.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-booter/2.22.2/surefire-booter-2.22.2.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-booter/2.22.2/surefire-booter-2.22.2.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/junit/junit/4.12/junit-4.12.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/junit/junit/4.12/junit-4.12.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/hamcrest/hamcrest-parent/1.3/hamcrest-parent-1.3.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/hamcrest/hamcrest-parent/1.3/hamcrest-parent-1.3.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-toolchain/2.2.1/maven-toolchain-2.2.1.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-toolchain/2.2.1/maven-toolchain-2.2.1.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-java/0.9.10/plexus-java-0.9.10.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-java/0.9.10/plexus-java-0.9.10.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-languages/0.9.10/plexus-languages-0.9.10.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-languages/0.9.10/plexus-languages-0.9.10.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus/4.0/plexus-4.0.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus/4.0/plexus-4.0.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/ow2/asm/asm/6.2/asm-6.2.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/ow2/asm/asm/6.2/asm-6.2.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/ow2/ow2/1.5/ow2-1.5.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/ow2/ow2/1.5/ow2-1.5.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/com/thoughtworks/qdox/qdox/2.0-M8/qdox-2.0-M8.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/com/thoughtworks/qdox/qdox/2.0-M8/qdox-2.0-M8.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/sonatype/oss/oss-parent/9/oss-parent-9.pom
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/sonatype/oss/oss-parent/9/oss-parent-9.pom (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/maven-surefire-common/2.22.2/maven-surefire-common-2.22.2.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-logger-api/2.22.2/surefire-logger-api-2.22.2.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-api/2.22.2/surefire-api-2.22.2.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/plugin-tools/maven-plugin-annotations/3.5.2/maven-plugin-annotations-3.5.2.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-plugin-api/2.2.1/maven-plugin-api-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/maven-surefire-common/2.22.2/maven-surefire-common-2.22.2.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-booter/2.22.2/surefire-booter-2.22.2.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-booter/2.22.2/surefire-booter-2.22.2.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-artifact/2.2.1/maven-artifact-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-plugin-api/2.2.1/maven-plugin-api-2.2.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/plugin-tools/maven-plugin-annotations/3.5.2/maven-plugin-annotations-3.5.2.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-artifact/2.2.1/maven-artifact-2.2.1.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/junit/junit/4.12/junit-4.12.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-logger-api/2.22.2/surefire-logger-api-2.22.2.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/surefire/surefire-api/2.22.2/surefire-api-2.22.2.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-plugin-descriptor/2.2.1/maven-plugin-descriptor-2.2.1.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-utils/1.5.15/plexus-utils-1.5.15.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-project/2.2.1/maven-project-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-utils/1.5.15/plexus-utils-1.5.15.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/junit/junit/4.12/junit-4.12.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-profile/2.2.1/maven-profile-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-artifact-manager/2.2.1/maven-artifact-manager-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-project/2.2.1/maven-project-2.2.1.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-settings/2.2.1/maven-settings-2.2.1.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/backport-util-concurrent/backport-util-concurrent/3.1/backport-util-concurrent-3.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-profile/2.2.1/maven-profile-2.2.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/backport-util-concurrent/backport-util-concurrent/3.1/backport-util-concurrent-3.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-plugin-descriptor/2.2.1/maven-plugin-descriptor-2.2.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-artifact-manager/2.2.1/maven-artifact-manager-2.2.1.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-core/2.2.1/maven-core-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-settings/2.2.1/maven-settings-2.2.1.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-plugin-parameter-documenter/2.2.1/maven-plugin-parameter-documenter-2.2.1.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-model/2.2.1/maven-model-2.2.1.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-interpolation/1.11/plexus-interpolation-1.11.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-plugin-registry/2.2.1/maven-plugin-registry-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-model/2.2.1/maven-model-2.2.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-interpolation/1.11/plexus-interpolation-1.11.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/slf4j/slf4j-api/1.5.6/slf4j-api-1.5.6.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-core/2.2.1/maven-core-2.2.1.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/slf4j/slf4j-jdk14/1.5.6/slf4j-jdk14-1.5.6.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/slf4j/jcl-over-slf4j/1.5.6/jcl-over-slf4j-1.5.6.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-plugin-registry/2.2.1/maven-plugin-registry-2.2.1.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-repository-metadata/2.2.1/maven-repository-metadata-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-plugin-parameter-documenter/2.2.1/maven-plugin-parameter-documenter-2.2.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/slf4j/slf4j-api/1.5.6/slf4j-api-1.5.6.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-monitor/2.2.1/maven-monitor-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/slf4j/slf4j-jdk14/1.5.6/slf4j-jdk14-1.5.6.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-toolchain/2.2.1/maven-toolchain-2.2.1.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/slf4j/jcl-over-slf4j/1.5.6/jcl-over-slf4j-1.5.6.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-error-diagnostics/2.2.1/maven-error-diagnostics-2.2.1.jar
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-java/0.9.10/plexus-java-0.9.10.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-error-diagnostics/2.2.1/maven-error-diagnostics-2.2.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/codehaus/plexus/plexus-java/0.9.10/plexus-java-0.9.10.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/com/thoughtworks/qdox/qdox/2.0-M8/qdox-2.0-M8.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-monitor/2.2.1/maven-monitor-2.2.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-toolchain/2.2.1/maven-toolchain-2.2.1.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/apache/maven/maven-repository-metadata/2.2.1/maven-repository-metadata-2.2.1.jar (0 B at 0 B/s)
+Downloading from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/ow2/asm/asm/6.2/asm-6.2.jar
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/com/thoughtworks/qdox/qdox/2.0-M8/qdox-2.0-M8.jar (0 B at 0 B/s)
+Downloaded from alimaven: https://maven.aliyun.com/nexus/content/groups/public/org/ow2/asm/asm/6.2/asm-6.2.jar (0 B at 0 B/s)
 [INFO] 
 [INFO] -------------------------------------------------------
 [INFO]  T E S T S
