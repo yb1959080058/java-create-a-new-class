a512d0534cd9c63037cb6ad345fcf72d95542faa
QianChiLang1 <1149956184@qq.com>
2022-10-03 15:16:30 +0000

    Update at 2022-10-03T15:16:30.555835Z

---
+++ src/main/java/com/bytelegend/Cat.java
@@ -0,0 +1,22 @@
+[INFO] 
+[INFO] -------------------------------------------------------
+[INFO]  T E S T S
+[INFO] -------------------------------------------------------
+[INFO] Running com.bytelegend.CatTest
+[INFO] Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.019 s - in com.bytelegend.CatTest
+[INFO] 
+[INFO] Results:
+[INFO] 
+[INFO] Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
+[INFO] 
+[INFO] 
+[INFO] --- maven-jar-plugin:2.4:jar (default-jar) @ java-create-a-new-class ---
+[INFO] Building jar: D:\programming\Java\java-create-a-new-class\target\java-create-a-new-class-1.0-SNAPSHOT.jar
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD SUCCESS
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time:  8.445 s
+[INFO] Finished at: 2022-10-03T23:00:52+08:00
+[INFO] ------------------------------------------------------------------------
+
+进程已结束，退出代码为 0
