c5cf2a4548b2831b31fa4e2e70435cb2f9d07a53
ovxrfl0w <48533092+ovxrfl0w@users.noreply.github.com>
2023-05-06 17:23:35 +0000

    Update at 2023-05-06T17:23:35.273328Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,10 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat() {
+        this("");
+    }
+
     public Cat(String name) {
         this.name = name;
     }
