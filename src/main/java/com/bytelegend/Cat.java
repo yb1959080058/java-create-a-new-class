86dc3cda8b89a72a26ac2a93ed04c661230ef487
ACodeHX <1468232850@qq.com>
2023-05-08 14:43:06 +0800

    revamp

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,5 +1,7 @@
-public class Cat {
-    public static void main(String[] args) {
+public class Cat
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hello,GitHub!");
     }
 }
