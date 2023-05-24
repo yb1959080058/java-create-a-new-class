9170ba78a4ba299d4b5962b81e6883be209815a5
guilherme-alves-silve <kurosaki_alves@hotmail.com>
2023-05-24 12:31:34 +0000

    Update at 2023-05-24T12:31:34.371608Z

---
+++ src/main/java/com/bytelegend/Cat.java
@@ -0,0 +1,21 @@
+public class Cat {
+    private final String name;
+    private final int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String toString() {
+        return "{name: " + name + ", age: " + age + "}";
+    }
+}
